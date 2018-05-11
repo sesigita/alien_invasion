c2f631e415553b402eeafe6f8d4aac4ca7c360cc
Sigita Senkiene <senkiene.sigita@gmail.com>
2018-05-11 12:19:51 +0300

    Refactor scoreboard.py, create prep_images()

--- scoreboard.py
+++ scoreboard.py
@@ -14,11 +14,7 @@ class Scoreboard():
     self.text_color = (30, 30, 30)
     self.font = pygame.font.SysFont(None, 48)
 
-    # prepare the initial scsore image
-    self.prep_score()
-    self.prep_high_score()
-    self.prep_level()
-    self.prep_ships()
+    self.prep_images()
 
   def prep_score(self):
     """ turn score into aimg """
@@ -64,3 +60,10 @@ class Scoreboard():
       ship.rect.x = 10 + ship_number * ship.rect.width
       ship.rect.y = 10
       self.ships.add(ship)
+  
+  def prep_images(self):
+    # prepare the initial scsore image
+    self.prep_score()
+    self.prep_high_score()
+    self.prep_level()
+    self.prep_ships()
